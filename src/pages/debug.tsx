4535fc780888911aade21bf4d88a842113d3bdb0
chenzhilong <chenzhilong@zhuozhuo.io>
2021-10-07 22:45:35 +0800

    update

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -6,9 +6,9 @@ function Debug() {
     <div className="p-debug">
       <div className="float">float</div>
       <div className="content">
-        {/* <h2>斐林试剂佛时间段佛为非见识到了福建安山东解耦if就爱上了对方</h2> */}
-        斐林试剂佛时间段佛为非见识到了福建安山东解耦if就爱上了对方
+        <h2>斐林试剂佛时间段佛为非见识到了福建安山东解耦if就爱上了对方</h2>
       </div>
+      <button onClick={()=>{}}>debug</button>
     </div>
   );
 }
